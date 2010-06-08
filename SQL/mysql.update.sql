53e3fe554bc867402c5bd9d8b1d58ae042baf762
Denny Lin <dennylin93@cnmc.tw>
2010-06-08 19:18:32 +0800

    Change names of keys. Keep them consistent with RC.
    Signed-off-by: Denny Lin <dennylin93@cnmc.tw>

--- SQL/mysql.update.sql
+++ SQL/mysql.update.sql
@@ -10,3 +10,14 @@ ALTER TABLE `rcguard`
 ALTER TABLE `rcguard`
   ADD INDEX (`last`),
   ADD INDEX (`hits`);
+
+-- 0.2.0 -> 0.2.1
+TRUNCATE TABLE `rcguard`;
+
+ALTER TABLE `rcguard`
+  DROP INDEX `last`,
+  DROP INDEX `hits`;
+
+ALTER TABLE `rcguard`
+  ADD INDEX `last_index` (`last`),
+  ADD INDEX `hits_index` (`hits`);
