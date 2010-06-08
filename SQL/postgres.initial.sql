91f6b153806936081e79ccdab30c7a401d856a8c
Denny Lin <dennylin93@cnmc.tw>
2010-06-08 19:11:04 +0800

    Cleanup SQL for PostgreSQL.
    Signed-off-by: Denny Lin <dennylin93@cnmc.tw>

--- SQL/postgres.initial.sql
+++ SQL/postgres.initial.sql
@@ -1,22 +1,10 @@
 -- PostgreSQL table for rcguard
 
 CREATE TABLE rcguard (
-	ip
-		varchar(40)
-		NOT NULL
-		PRIMARY KEY,
-	first
-		timestamp with time zone
-		NOT NULL,
-	last
-		timestamp with time zone
-		NOT NULL,
-	hits
-		integer
-		NOT NULL
+	ip varchar(40) NOT NULL PRIMARY KEY,
+	first timestamp with time zone NOT NULL,
+	last timestamp with time zone NOT NULL,
+	hits integer NOT NULL
 );
 
-CREATE INDEX
-	indx_rcguard_last
-ON
-	rcguard(last);
+CREATE INDEX indx_rcguard_last ON rcguard(last);
