9ac9d101e32477f6f7fe4bc79353c5e13394ea2e
Diana Soares <diana.soares@gmail.com>
2018-05-11 13:00:06 +0100

    Remove unneeded rcguard.js.

--- rcguard.php
+++ rcguard.php
@@ -233,7 +233,6 @@ class rcguard extends rcube_plugin
         }
 
         $this->include_stylesheet($skin_path . '/rcguard.css');
-        $this->include_script('rcguard.js');
 
         $recaptcha_api = ($rcmail->config->get('recaptcha_https') || rcube_utils::https_check()) ?
             $rcmail->config->get('recaptcha_api_secure') : $rcmail->config->get('recaptcha_api');
