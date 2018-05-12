ef4ba5f594fa25057f9485efd636dce9ef8380cd
Diana Soares <diana.soares@gmail.com>
2018-05-12 14:08:15 +0100

    Release version 1.1.1

--- rcguard.php
+++ rcguard.php
@@ -4,7 +4,7 @@
  *
  * Roundcube plugin to provide Google reCAPTCHA service to Roundcube.
  *
- * @version 1.1.0
+ * @version 1.1.1
  * @author Diana Soares
  *
  * Copyright (c) 2010-2012 Denny Lin. All rights reserved.
